4e249effb30b99e46b629edaf838e72e26bbb11e
Seregon <109359355+seregonwar@users.noreply.github.com>
2024-11-15 21:49:52 +0100

    Create extension.js

---
+++ src/extension.js
@@ -0,0 +1,12 @@
+const vscode = require('vscode');
+
+function activate(context) {
+    console.log('MetaForge Syntax Highlighting Activated');
+}
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+};
